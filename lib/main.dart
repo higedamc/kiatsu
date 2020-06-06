927a3e9c46e8a5adc1bb7319aef98dd7fecf9f07
higedamc <jstache@tutanota.com>
2020-06-06 18:03:22 +0900

    API取得方法分離 + いらないもの色々削除。

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http;
-import 'package:kiatsu/weather_model.dart';
+import 'package:kiatsu/process/api_getter.dart';
 import 'package:share/share.dart';
-import 'package:weather/weather_library.dart';
+
+import 'model/weather_model.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,77 +13,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-//  Weather w2;
-//  String _res = 'にゃーん';
+  final ApiGetter getData = ApiGetter();
   String _res2 = "ちんちん";
-  String key = '85b471dd6643e05717257b12894250d1';
-  WeatherStation ws;
-  int res_p = 0;
-//  WeatherStation ws;
-//  int res_p = 0;
 
   @override
   void initState() {
     super.initState();
-//    ws = new WeatherStation(key);
-//    initPlatformState();
   }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-//  Future<void> initPlatformState() async {
-//    queryWeather();
-////    queryBarometer();
-//  }
-  /*
-  Future<void> _onRefresh() async {
-    print('future');
-    queryWeather();
-    queryBarometer();
-    queryForecast();
-  }
-   */
-
-  Future<WeatherClass> getWeather() async {
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    String url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
-        position.latitude.toString() +
-        '&lon=' +
-        position.longitude.toString() +
-        '&APPID=$key';
-    final response = await http.get(url);
-    return WeatherClass.fromJson(json.decode(response.body));
-  }
-
-//  void queryForecast() async {
-//    List<Weather> f = await ws.fiveDayForecast();
-//    setState(() {
-//      _res = f.toString();
-//    });
-//  }
-
-//  void queryWeather() async {
-////    Weather w = await ws.currentWeather(latitude, longitude);
-//    Weather w = (await getWeather()) as Weather;
-//    setState(() {
-//      _res = w.toString();
-//      print('weather api test*****************************');
-//      print(_res);
-//    });
-//  }
-
-//  void queryBarometer() async {
-//    Weather w2 = await ws.currentWeather(latitude, longtitude);
-//    double pressure = w2.pressure.toDouble();
-//    setState(() {
-//      _res2 = w2.toString();
-//      res_p = pressure.toInt();
-//      print('pressure *****************');
-//      print(w2);
-//      print('pressure *****************');
-//      print(pressure);
-//    });
-//  }
 
   @override
   Widget build(BuildContext context) {
@@ -95,7 +28,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: FutureBuilder<WeatherClass>(
-          future: getWeather(),
+          future: getData.getWeather(),
           builder: (context, snapshot) {
             if (snapshot.hasError) print(snapshot.error);
             return snapshot.hasData
@@ -118,7 +51,7 @@ class _MyAppState extends State<MyApp> {
                       ],
                     ),
                     body: FutureBuilder<WeatherClass>(
-                        future: getWeather(),
+                        future: getData.getWeather(),
                         builder: (context, snapshot) {
                           if (snapshot.hasError) print(snapshot.error);
                           return snapshot.hasData
