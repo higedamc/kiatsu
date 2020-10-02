3823af64fe61330caf143cc8c8b0eb09a57bd326
HigeMan <jstache@tutanota.com>
2020-10-03 04:07:20 +0900

    はっ

--- .github/scripts/decrypt_secret_test.sh
+++ .github/scripts/decrypt_secret_test.sh
@@ -8,9 +8,10 @@ export GPG_TTY=$(tty)
 # source ~/.zshrc
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew install pinentry-mac
-echo "pinentry-program `which pinentry-mac`" > $HOME/.gnupg/gpg-agent.conf
-printf "$GPG_SIGNING_KEY" | base64 --decode > $HOME/.gnupg/private.key
-gpg --import $HOME/.gnupg/private.key
+sudo gpgconf --kill dirmngr
+echo "pinentry-program `which pinentry-mac`" > ~/.gnupg/gpg-agent.conf
+printf "$GPG_SIGNING_KEY" | base64 --decode > ~/.gnupg/private.key
+gpg --import ~/.gnupg/private.key
 # printenv | grep $SECRETS_PASSPHRASE
 # gpg --quiet --batch --yes --passphrase="$SECRETS_PASSPHRASE" --output lib/env/production_secrets.dart --decrypt lib/env/production_secrets.dart.gpg
 echo $SECRETS_PASSPHRASE | gpg --passphrase-fd 0 --output lib/env/production_secrets.dart --decrypt --batch lib/env/production_secrets.dart.gpg
