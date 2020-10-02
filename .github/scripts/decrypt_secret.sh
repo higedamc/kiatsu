0be2418725dd91e5ba1553e38c47462946441d89
HigeMan <jstache@tutanota.com>
2020-10-02 21:27:08 +0900

    E

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,13 +2,13 @@
 sudo gpgconf --kill dirmngr
 # sudo chown -R $USER:$USER ~/.gnupg
 mkdir -p $HOME/secrets
-mkdir -p $HOME/.gnupg
+mkdir -p $HOME/.gnupg/
 # touch $HOME/.gnupg/gpg.conf
 # touch $HOME/.gnupg/gpg-agent.conf
 # echo 'user-agent' > $HOME/.gnupg/gpg.conf
 # echo 'pinentry-mode loopback' > $HOME/.gnupg/gpg-agent.conf
 # echo 'allow-loopback-pinentry' >> $HOME/.gnupg/gpg-agent.conf
 # echo RELOADAGENT | gpg-connect-agent
-printf "$GPG_SIGNING_KEY" | base64 --decode > $HOME/secrets/private.key
-gpg --import $HOME/secrets/private.key
+printf "$GPG_SIGNING_KEY" | base64 --decode > $HOME/.gnupg/private.key
+gpg --import $HOME/.gnupg/private.key
 # gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" ¥ --output lib/env/production_secrets.dart lib/env/production_secrets.dart.gpg
