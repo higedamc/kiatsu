c4954a1164f1f6ffb36f7c33a70b26851734b927
HigeMan <jstache@tutanota.com>
2020-10-02 20:51:17 +0900

    a

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -8,4 +8,4 @@ mkdir $HOME/secrets
 # echo RELOADAGENT | gpg-connect-agent
 # printf "$GPG_SIGNING_KEY" | base64 --decode > $HOME/secrets/private.key
 gpg --import "$BLACKBOX_PRIVKEY"
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" --output lib/env/production_secrets.dart lib/env/production_secrets.dart.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" ¥ --output lib/env/production_secrets.dart lib/env/production_secrets.dart.gpg
