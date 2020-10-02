39f1b4399ceb6ef6e7d8844c26710a822aa9e741
HigeMan <jstache@tutanota.com>
2020-10-02 22:15:08 +0900

    For macOS

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,16 +2,19 @@
 sudo gpgconf --kill dirmngr
 # sudo chown -R $USER:$USER ~/.gnupg
 mkdir -p $HOME/secrets
-mkdir -p $HOME/.gnupg/
-echo 'GPG_TTY=$(tty)' > ~/.bashrc
-echo 'export GPG_TTY' >> ~/.bashrc
-source ~/.bashrc
+mkdir -p $HOME/.gnupg/  
+echo 'GPG_TTY=$(tty)' > $HOME/.bashrc
+echo 'export GPG_TTY' >> $HOME/.bashrc
+source $HOME/.bashrc
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install gnupg pinentry-mac
 # touch $HOME/.gnupg/gpg.conf
 # touch $HOME/.gnupg/gpg-agent.conf
 # echo 'user-agent' > $HOME/.gnupg/gpg.conf
 # echo 'pinentry-mode loopback' > $HOME/.gnupg/gpg-agent.conf
 # echo 'allow-loopback-pinentry' >> $HOME/.gnupg/gpg-agent.conf
-# echo RELOADAGENT | gpg-connect-agent
+echo "pinentry-program `which pinentry-mac`" > ~/.gnupg/gpg-agent.conf
+gpgconf --kill gpg-agent
 printf "$GPG_SIGNING_KEY" | base64 --decode > $HOME/.gnupg/private.key
 gpg --import $HOME/.gnupg/private.key
 # printenv | grep $SECRETS_PASSPHRASE
