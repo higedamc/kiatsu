ad24c7aea4bc09669fa3f16ce8344af6a8b7b601
HigeMan <jstache@tutanota.com>
2020-10-02 20:47:35 +0900

    Simplify

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -6,6 +6,6 @@ mkdir $HOME/secrets
 # echo 'pinentry-mode loopback' > ~/.gnupg/gpg-agent.conf
 # echo 'allow-loopback-pinentry' >> ~/.gnupg/gpg-agent.conf
 # echo RELOADAGENT | gpg-connect-agent
-printf "$GPG_SIGNING_KEY" | base64 --decode > $HOME/secrets/private.key
-gpg --import < $HOME/secrets/private.key
+# printf "$GPG_SIGNING_KEY" | base64 --decode > $HOME/secrets/private.key
+gpg --import "$BLACKBOX_PRIVKEY"
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" --output lib/env/production_secrets.dart lib/env/production_secrets.dart.gpg
