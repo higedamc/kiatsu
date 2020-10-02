14ccab43e7afc7e2b847887327f68937b65ae4d8
HigeMan <jstache@tutanota.com>
2020-10-03 03:05:39 +0900

    うーむ

--- .github/scripts/decrypt_secret_test.sh
+++ .github/scripts/decrypt_secret_test.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -eo pipefail
 sudo gpgconf --kill dirmngr
-printf "$GPG_SIGNING_KEY" | base64 --decode > ./.github/secrets/private.key
-gpg --import ./.github/secrets/private.key
+mkdir -p $HOME/.gnupg/
+printf "$GPG_SIGNING_KEY" | base64 --decode > $HOME/.gnupg/private.key
+gpg --import $HOME/.gnupg/private.key
 # printenv | grep $SECRETS_PASSPHRASE
 # gpg --quiet --batch --yes --passphrase="$SECRETS_PASSPHRASE" --output lib/env/production_secrets.dart --decrypt lib/env/production_secrets.dart.gpg
 echo $SECRETS_PASSPHRASE | gpg --passphrase-fd 0 --output lib/env/production_secrets.dart --decrypt --batch lib/env/production_secrets.dart.gpg
