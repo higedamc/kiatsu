b8647cea036fb1ed95e1f139ac4625ffc805b6a3
HigeMan <jstache@tutanota.com>
2020-10-02 22:53:40 +0900

    iOS用とAndroid用を分離

---
+++ .github/scripts/decrypt_secret_ubuntu.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mkdir -p $HOME/secrets
+mkdir -p $HOME/.gnupg/
+printf "$GPG_SIGNING_KEY" | base64 --decode > $HOME/.gnupg/private.key
+gpg --import $HOME/.gnupg/private.key
+echo $SECRETS_PASSPHRASE | gpg --passphrase-fd 0 --output lib/env/production_secrets.dart --decrypt --batch lib/env/production_secrets.dart.gpg
