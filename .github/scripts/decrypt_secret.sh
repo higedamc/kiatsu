071825bfcc4ff09548f059644c1d73d3a754bf68
HigeMan <jstache@tutanota.com>
2020-10-02 22:02:37 +0900

    macOS

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -11,6 +11,6 @@ mkdir -p $HOME/.gnupg/
 # echo RELOADAGENT | gpg-connect-agent
 printf "$GPG_SIGNING_KEY" | base64 --decode > $HOME/.gnupg/private.key
 gpg --import $HOME/.gnupg/private.key
-printenv | grep $SECRETS_PASSPHRASE
+# printenv | grep $SECRETS_PASSPHRASE
 # gpg --quiet --batch --yes --passphrase="$SECRETS_PASSPHRASE" --output lib/env/production_secrets.dart --decrypt lib/env/production_secrets.dart.gpg
 echo $SECRETS_PASSPHRASE | gpg --passphrase-fd 0 --output lib/env/production_secrets.dart --decrypt --batch lib/env/production_secrets.dart.gpg
