cb65791419e5e1d3d27d345d88bd570c0bb0b452
HigeMan <jstache@tutanota.com>
2022-04-03 23:46:11 +0900

    - refactor: 不必要なコメントアウトを削除 - refactor: 不必要な変数を削除

--- lib/pages/subscriptions_page.dart
+++ lib/pages/subscriptions_page.dart
@@ -1,28 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kiatsu/api/purchase_api.dart';
-import 'package:kiatsu/const/enumnum.dart';
-import 'package:kiatsu/controller/progress_controller.dart';
-import 'package:kiatsu/controller/purchase_controller.dart';
 import 'package:kiatsu/controller/user_controller.dart';
-import 'package:kiatsu/model/entitlement.dart';
-import 'package:kiatsu/pages/timeline.dart';
 import 'package:kiatsu/providers/providers.dart';
-// import 'package:kiatsu/providers/providers.dart' as pro;
-import 'package:kiatsu/providers/revenuecat.dart';
-import 'package:kiatsu/providers/scaffold_messanger_provider.dart';
-import 'package:kiatsu/utils/navigation_service.dart';
-import 'package:kiatsu/utils/purchase_manager.dart';
 import 'package:kiatsu/utils/utils.dart';
 import 'package:kiatsu/widget/paywall_widget.dart';
-import 'package:provider/provider.dart' as pro;
 import 'package:purchases_flutter/purchases_flutter.dart';
 
 final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
@@ -30,58 +15,14 @@ final FirebaseFirestore firebaseStore = FirebaseFirestore.instance;
 final currentUser = firebaseAuth.currentUser;
 final CollectionReference users = firebaseStore.collection('users');
 
-//TODO: #117 iOS版のサブスク機能が動くようにする
-//TODO: #116 課金後課金情報が消えてしまうので課金情報を更新する
-
-// class Coins {
-//   // Entitlementsの設定
-//   // static const removeAds = 'kiatsu_120_remove_ads';
-//   // for iOS
-//   final removeAdsAndroid = 'kiatsu_120_remove_ads';
-//   final tipMe = 'tip_me';
-//   final subsc1m = 'kiatsu_pro_1m';
-//   final subsc1y = 'kiatsu_pro_1y';
-//   final _apiKey = dotenv.env['REVENUECAT_SECRET_KEY'].toString();
-//   // Added some
-//   static const allIds = [removeAdsAndroid, tipMe, subsc1m, subsc1y];
-// }
-
-// class SubscriptionsPage extends StatefulWidget {
-//   const SubscriptionsPage({Key? key}) : super(key: key);
-
-//   @override
-//   _SubscriptionsPageState createState() => _SubscriptionsPageState();
-// }
-
 class SubscriptionsPage extends ConsumerWidget {
-  SubscriptionsPage({Key? key}) : super(key: key);
-  bool isLoading = false;
+  const SubscriptionsPage({Key? key}) : super(key: key);
+  bool get isLoading => false;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final uid = ref.watch(userProvider.select((s) => s.user?.uid));
-    final appUserId = ref.watch(userProvider.select((s) => s.appUserId));
     final user = ref.watch(authStateChangesProvider).asData?.value;
-    final activeEntitlements =
-        ref.watch(userProvider.select((s) => s.activeEntitlements));
     final isNoAds = ref.watch(userProvider.select((s) => s.isNoAdsUser));
-    final products = ref.watch(purchaseProvider.select((s) => s.products));
-    // final entitlement = pro.Provider.of<RevenueCat>(context).entitlement;
-
-    // RevenueCat().updatePurchaseStatus();
-    // final current = PurchaseApi.getCurrentPurchaser();
-
-    // print(current.toString());
-    // final entitlement = ref.watch(revenueCatProvider).entitlement;
-    // final _purchaser = ref.watch(purchaseManagerProvider);
-    // Future<void> waiter() async {
-    //   return Future.delayed(Duration.zero, () async {
-    //     // PurchaseApi.init();
-    //     await Purchases.setup(Coins._apiKey,
-    //         appUserId: currentUser?.uid.toString());
-    //   });
-    // }
-
     Future<void> fetchOffers() async {
       final offerings = await PurchaseApi.fetchOffers(all: true);
 
@@ -103,15 +44,8 @@ class SubscriptionsPage extends ConsumerWidget {
             description: '今後色々なアンロックできる特典を追加していく予定です！',
             onClickedPackage: (package) async {
               await PurchaseApi.purchasePackage(package);
-              // if (user != null) {
-              //   return users.doc(currentUser?.uid).update({
-              //   'entitlement': _purchaser.entitlement.name,
-              // });
-              // }
-             await users.doc(user?.uid).set({'isPurchased': true});
+              await users.doc(user?.uid).set({'isPurchased': true});
               Navigator.pop(context);
-
-              // Navigator.pop(context);
             },
           ),
         );
@@ -129,25 +63,6 @@ class SubscriptionsPage extends ConsumerWidget {
             Text(text, style: const TextStyle(fontSize: 24)),
           ],
         );
-
-    // Widget buildEntitlement(Entitlement entitlement) {
-    //   switch (entitlement) {
-    //     case Entitlement.pro:
-    //       return buildEntitlementIcon(
-    //         text: '有料プラン利用中',
-    //         icon: Icons.done, // ex. paid
-    //       );
-    //     case Entitlement.free:
-       
-    //       return buildEntitlementIcon(
-    //         text: '無料プラン利用中',
-    //         icon: Icons.lock,
-    //       );
-    //   }
-    // }
-
-    
-
     return Scaffold(
       appBar: NeumorphicAppBar(
         title: const Text('サブスクリプション'),
@@ -188,24 +103,8 @@ class SubscriptionsPage extends ConsumerWidget {
                       style: TextStyle(fontSize: 20),
                     ),
                     onPressed: () async {
-                      // final p = products;
-                      // final errorCode = await ref
-                      //       .read(progressController)
-                      //       .executeWithProgress(
-                      //         () => ref
-                      //             .read(purchaseProvider.notifier)
-                      //             .purchaseProduct('pro'),
-                      //       );
-                      //   if (errorCode != null) {
-                      //     ref
-                      //         .read(scaffoldMessengerProvider)
-                      //         .currentState!
-                      //         .showAfterRemoveSnackBar(
-                      //             message: errorCode.described);
-                      //   }
                       await fetchOffers();
-                    }
-                  ),
+                    }),
             const SizedBox(height: 32),
             isNoAds
                 ? const Center()
@@ -228,7 +127,7 @@ class SubscriptionsPage extends ConsumerWidget {
                       if (restoredInfo.entitlements.all['pro'] != null &&
                           restoredInfo.entitlements.all['pro']!.isActive) {
                         // 復元完了のポップアップ
-                        final result = await showDialog<int>(
+                        await showDialog<int>(
                           context: context,
                           barrierDismissible: false,
                           builder: (BuildContext context) {
@@ -237,11 +136,14 @@ class SubscriptionsPage extends ConsumerWidget {
                               content: const Text('復元が完了しました。'),
                               actions: <Widget>[
                                 ElevatedButton(
-                                    child: const Text('OK',
-                                    style: TextStyle(color: Colors.black),),
-                                    onPressed: () async {
-                                      Navigator.of(context).pop(1);
-                                    }),
+                                  child: const Text(
+                                    'OK',
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                  onPressed: () async {
+                                    Navigator.of(context).pop(1);
+                                  },
+                                ),
                               ],
                             );
                           },
@@ -253,9 +155,10 @@ class SubscriptionsPage extends ConsumerWidget {
                           barrierDismissible: false,
                           builder: (BuildContext context) {
                             return AlertDialog(
-                              title: Text('確認'),
-                              content:
-                                  Text('過去の購入情報が見つかりませんでした。アカウント情報をご確認ください。'),
+                              title: const Text('確認'),
+                              content: const Text(
+                                '過去の購入情報が見つかりませんでした。アカウント情報をご確認ください。',
+                              ),
                               actions: <Widget>[
                                 ElevatedButton(
                                   child: const Text('OK'),
@@ -268,30 +171,6 @@ class SubscriptionsPage extends ConsumerWidget {
                       }
                     }),
             const SizedBox(height: 32),
-            // ElevatedButton(
-            //   style: ElevatedButton.styleFrom(
-            //     minimumSize: const Size.fromHeight(50),
-            //   ),
-            //   child: const Text(
-            //     '他の機能を見てみる',
-            //     style: TextStyle(fontSize: 20),
-            //   ),
-            //   onPressed: isLoading ? null : fetchOffers2,
-            // ),
-            // ElevatedButton(
-            //     style: ElevatedButton.styleFrom(
-            //       minimumSize: const Size.fromHeight(50),
-            //     ),
-            //     child: const Text(
-            //       'Get UserInfo',
-            //       style: TextStyle(fontSize: 20),
-            //     ),
-            //     onPressed: () async {
-            //       // await RevenueCat().updatePurchaseStatus();
-            //       // final current = await Purchases.getPurchaserInfo();
-            //       // print(current.toString());
-            //       // await waiter();
-            //     }),
           ],
         ),
       ),
