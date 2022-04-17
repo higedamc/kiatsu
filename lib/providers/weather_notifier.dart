a6167a848b3c544bfec81efb7c1b3e3b433641c2
HigeMan <jstache@tutanota.com>
2022-04-18 06:06:23 +0900

    - refactor: linter適用

--- lib/providers/weather_notifier.dart
+++ lib/providers/weather_notifier.dart
@@ -1,20 +1,22 @@
 
 
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kiatsu/api/api_state.dart';
 import 'package:kiatsu/repository/weather_repository.dart';
 
 class WeatherStateNotifer extends StateNotifier<WeatherClassState> {
-  final WeatherRepository weatherRepository;
   WeatherStateNotifer(this.weatherRepository)
       : super(const WeatherClassState.initial());
+  final WeatherRepository weatherRepository;
+  
 
   Future<void> getWeather(String cityName) async {
     try {
       state = const WeatherClassState.loading();
-      var data = await weatherRepository.getWeather(cityName);
+      final data = await weatherRepository.getWeather(cityName);
       state = WeatherClassState.success(data);
-    } catch (e) {
+    } on PlatformException catch (e) {
       state = WeatherClassState.error('$e');
     }
   }
