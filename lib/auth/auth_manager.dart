6beeca63228f3955f21ae54737aea98a4700c918
HigeMan <kohei.otani@tutanota.com>
2022-09-04 03:30:35 +0900

    - update: added supabaseAuth on AuthManager

--- lib/auth/auth_manager.dart
+++ lib/auth/auth_manager.dart
@@ -1,8 +1,11 @@
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_line_sdk/flutter_line_sdk.dart';  // LINE SDK追加
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:cloud_functions/cloud_functions.dart'; // cloud_functions追加
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:supabase_flutter/supabase_flutter.dart'; // cloud_functions追加
 
 final authManagerProvider = ChangeNotifierProvider<AuthManager>(
   (ref) {
@@ -13,11 +16,14 @@ final authManagerProvider = ChangeNotifierProvider<AuthManager>(
 class AuthManager with ChangeNotifier {
   AuthManager() {
     _firebaseAuth.authStateChanges().listen((user) {
-      isLoggedIn = user != null;
+      if(user !=null) {
+        setAuth(user.uid);
+      }
       notifyListeners();
     });
   }
   final _firebaseAuth = FirebaseAuth.instance;
+  final _supabaseAuth = Supabase.instance;
   // final _lineSdk = LineSDK.instance; // LINE SDKのインスタンス
   bool isLoggedIn = false;
   // ログイン処理
@@ -42,5 +48,27 @@ class AuthManager with ChangeNotifier {
   // LINE, Firebase両方でログアウトする
     // await _lineSdk.logout();
     await _firebaseAuth.signOut();
+    await _supabaseAuth.client.auth.signOut();
+  }
+  
+  void setAuth(String uid) {
+    const secret = 'wSaQ++TBdVQ2e6tA895SWQuMsOefEt2dbHsNlTu8c3/GLdsTo5XiKdHKPxsRqqtjgVdLV3AuYnFKyc7d409eiQ==';
+    try {
+      final jwt = JWT(
+        {
+          'uid': uid,
+          'role': 'authenticated',
+        }
+      );
+      final token = jwt.sign(
+        SecretKey(secret),
+        expiresIn: const Duration(hours: 1),
+      );
+      _supabaseAuth.client.auth.setAuth(token);
+    } on Exception catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
   }
 }
