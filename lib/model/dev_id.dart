8a7f326a9b0b68253e6b88c47594079216a29474
HigeMan <kohei.otani@tutanota.com>
2022-09-04 02:28:21 +0900

    - update: refactor devIds

--- lib/model/dev_id.dart
+++ lib/model/dev_id.dart
@@ -3,6 +3,4 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class DevIds {
   String get dev1 => dotenv.env['DEV_ID_1'].toString();
   String get dev2 => dotenv.env['DEV_ID_2'].toString();
-  String get dev3 => dotenv.env['DEV_ID_3'].toString();
-  String get dev4 => dotenv.env['DEV_ID_4'].toString();
 }
