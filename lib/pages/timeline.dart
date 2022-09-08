24ed85e5b567e477df3f1bff6ed9b904bb73a40c
HigeMan <kohei.otani@tutanota.com>
2022-09-08 10:49:06 +0900

    - update: 一旦投稿単位でブロックする機能の追加

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,20 +1,16 @@
-import 'dart:convert';
-import 'dart:io';
+import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart' as neu;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:kiatsu/auth/auth_manager.dart';
 import 'package:kiatsu/controller/user_controller.dart';
 import 'package:kiatsu/model/dev_id.dart';
-import 'package:kiatsu/pages/custom_dialog_box.dart';
 import 'package:kiatsu/providers/providers.dart';
-import 'package:kiatsu/widget/report_dialog_box.dart';
 import 'package:lottie/lottie.dart';
 import 'package:wiredash/wiredash.dart';
 
@@ -26,7 +22,15 @@ final FirebaseFirestore firebaseStore = FirebaseFirestore.instance;
 final currentUser = firebaseAuth.currentUser;
 final uid = firebaseAuth.currentUser?.uid;
 final CollectionReference users = firebaseStore.collection('users');
-final CollectionReference<Map<String, dynamic>> test =
+// final comments = firebaseStore.collectionGroup('comments');
+final CollectionReference blocked = firebaseStore.collection('blockedUsers');
+// final stream = FirebaseFirestore.instance.collection('users')
+//   // .orderBy('isBlocked', descending: false)
+//   // .snapshots();
+//   .where('isBlockedBy', arrayContains: uid);
+// final docShot = firebaseStore.collection('isBlocked').where('isBlockedBy', arrayContains: uid).snapshots();
+// ログインユーザーのコメント群
+final CollectionReference<Map<String, dynamic>> comment =
     users.doc(uid).collection('comments');
 
 class Timeline extends ConsumerWidget {
@@ -46,6 +50,7 @@ class Timeline extends ConsumerWidget {
     final authManager = ref.watch(authManagerProvider);
     final isNoAds = ref.watch(userProvider.select((s) => s.isNoAdsUser));
     final createdAt = ref.watch(clockProvider);
+    final isBlocked = ref.watch(blockedOrNotProvider);
     final size = MediaQuery.of(context).size;
     if (kDebugMode) {
       print(size);
@@ -55,7 +60,6 @@ class Timeline extends ConsumerWidget {
     final currentWidth = width / 100;
     final currentHeight = height / 100;
     final txtControllerProvider = ref.watch(textControllerStateProvider);
-    final blockListProvider = ref.watch(blockIdCollectionStreamProvider);
     return Scaffold(
       appBar: neu.NeumorphicAppBar(
         title: const Text('お気持ち投稿の場'),
@@ -73,8 +77,35 @@ class Timeline extends ConsumerWidget {
                       ? GestureDetector(
                           // ignore: avoid_dynamic_calls
                           child: Visibility(
-                            visible: !(data[index]['isHidden'] == true),
-                            // visible: !(blockListProvider.value?.contains(currentUser?.uid) ?? false),
+                            //TODO: commentのisBlockedByにログインユーザーのuidが含まれている場合は表示しない
+                            // visible: data[index]['isBlockedBy']
+                            //     .contains(currentUser!.uid) == true,
+                            // visible: !(data[index]['commentId'] == true),
+                            // visible: !(blockListProvider.asData?.value
+                            //         .contains(data[index]['userId']) ==
+                            //     false),
+                            // visible: data[index]['userId'].toString() == docShot.contains(data[index]['userId']).toString(),
+                            // visible: (
+                            // ignore: avoid_dynamic_calls
+                            //  blocks['${data[index]['blockId']}'] == null
+                            // users
+                            // .doc(currentUser?.uid).collection('blocks').
+                            // .doc(data[index]['uid']).get().then(
+                            //       (value) => value.exists,
+                            //     ),
+
+                            // ),
+                            //TODO: とりあえず投稿単位ではブロックできる
+                            visible: isBlocked == false
+                            // ,
+                             && data[index]['isHidden'] == false,
+                            // visible: !(users.),
+                            // visible: !(blockListProvider.value
+                            //         ?.contains(data[index]['userId']) ??
+                            //     false),
+
+                            // visible: firebaseStore.doc('blocked/$uid').snapshots().contains(data[index]['userId']) == true,
+                            // visible: docShot.where((event) => event.data()!.containsKey(data[index]['$uid'])) == true,
                             child: Card(
                               shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(30),
@@ -92,7 +123,7 @@ class Timeline extends ConsumerWidget {
                                       color: Colors.red[700],
                                       icon: Icons.delete,
                                       onTap: () async {
-                                        await test
+                                        await comment
                                             .doc(
                                               data[index]['commentId']
                                                   .toString(),
@@ -104,7 +135,9 @@ class Timeline extends ConsumerWidget {
                                     ),
                                 ],
                                 secondaryActions: <Widget>[
-                                  IconSlideAction(
+                               if (data[index]['userId']
+                                          ?.contains(currentUser?.uid) ==
+                                      false)  IconSlideAction(
                                     caption: '通報・ブロック',
                                     color: Colors.blueGrey,
                                     icon: Icons.block,
@@ -158,45 +191,124 @@ class Timeline extends ConsumerWidget {
                                                           ),
                                                           onTap: () async {
                                                             //TODO: ここにブロックの処理を書く
-                                                            // await test
+                                                            // if (kDebugMode) {
+                                                            //   print(docShot);
+                                                            // }
+                                                            // final yesss = await docShot.;
+                                                            // if (kDebugMode) {
+                                                            //   print(yesss);
+                                                            // }
+                                                            // final mapData = <
+                                                            //     String,
+                                                            //     dynamic>{
+                                                            //   'comment':
+                                                            //       txtControllerProvider
+                                                            //           .text,
+                                                            //   'createdAt':
+                                                            //       createdAt,
+                                                            //   'userId':
+                                                            //       currentUser!
+                                                            //           .uid
+                                                            // };
+                                                            //TODO: 後で試す
+                                                            // final mapData = <
+                                                            //     String,
+                                                            //     dynamic>{
+                                                            //   'blockId':
+                                                            //       currentUser!
+                                                            //           .uid,
+                                                            //   'createdAt':
+                                                            //       createdAt,
+                                                            //   'userId':
+                                                            //       data[index][
+                                                            //               'userId']
+                                                            //           .toString(),
+                                                            // };
+                                                            // await
+                                                            //     users.doc(currentUser?.uid).collection('blocks').add(
+                                                            //           mapData
+                                                            // );
+                                                            final maps = <
+                                                                String,
+                                                                dynamic>{
+                                                              data[index]
+                                                                      ['userId']
+                                                                  .toString(): true
+                                                            };
+                                                            // await blocks.doc(currentUser?.uid).set(
+                                                            //    maps
+                                                            // );
+                                                            // await test2
+                                                            //     .doc(currentUser!.uid)
+                                                            //     .update({
+                                                            //   data[index][
+                                                            //           'userId']
+                                                            //       .toString(): true,
+                                                            // });
+                                                            // final mapData = <
+                                                            //     String,
+                                                            //     dynamic>{
+                                                            //   'isHidden':
+                                                            //       true
+                                                            // };
+                                                            // await comment
                                                             //     .doc(
                                                             //       data[index][
                                                             //               'isHidden']
                                                             //           .toString(),
-                                                            //     )
-                                                            //     .update({});
-                                                            final mapData = <
-                                                                String,
-                                                                dynamic>{
-                                                              'comment':
-                                                                  txtControllerProvider
-                                                                      .text,
-                                                              'createdAt':
-                                                                  createdAt,
-                                                              'userId':
-                                                                  currentUser!
-                                                                      .uid
-                                                            };
-
-                                                            final docRef = await users
-                                                                .doc(currentUser
-                                                                    ?.uid)
-                                                                .collection(
-                                                                    'comments')
-                                                                .add(mapData);
-                                                            final documentId =
-                                                                docRef.id;
-
-                                                            await users
-                                                                .doc(currentUser
-                                                                    ?.uid)
-                                                                .collection(
-                                                                    'comments')
-                                                                .doc(documentId)
+                                                            //     ).set(true)
+                                                            //     // .set(mapData,
+                                                            //     //     SetOptions(
+                                                            //     //         merge:
+                                                            //     //             true))
+                                                            //     ;
+                                                            // await users.doc(data[index]['isHidden'].toString()).update(maps);
+                                                            //TODO: これは動く
+                                                            // await firebaseStore
+                                                            //     .doc(
+                                                            //         'blocked/$uid')
+                                                            //     .update({
+                                                            //   data[index]
+                                                            //           ['userId']
+                                                            //       .toString(): true
+                                                            // });
+                                                            //TODO: arrayUnionしたい
+                                                            // final  dummyToken = {"dummy": 'dummy', "isBlockedBy": data[index]['userId'].toString()} as List<dynamic>;
+                                                            // await firebaseStore
+                                                            //     .doc(
+                                                            //         'isBlocked/$uid')
+                                                            //     .update({
+                                                            //       'isBlockedBy': FieldValue.arrayUnion(
+                                                            //         dummyToken
+                                                            //       )
+                                                            // });
+                                                            // await users.get();
+                                                            final DocumentReference docRefs = users.doc(data[index]['userId'].toString());
+                                                            // final DocumentReference docRefs2 = blocked.doc(data[index]['userId'].toString());
+                                                            final foo = ['dummy', currentUser?.uid];
+                                                            await docRefs
                                                                 .update({
-                                                                  'isHidden': true
+                                                              'isHidden': true,
+                                                              'isBlockedBy': FieldValue.arrayUnion(foo),
+                                                            });
+                                                            final getDocRefs = await docRefs.get();
+                                                            final Map<String, dynamic> baka = getDocRefs.data() as Map<String, dynamic>;
+                                                            final result = baka['isBlockedBy'] as List<dynamic>;
+                                                            final result2 = result.where((element) => element != 'dummy').toList();
+                                                            result2.isNotEmpty ? log('This shit is blocked!') : log('Aint blocked yet');
+                                                            await docRefs.collection('comments').doc(data[index]['commentId'].toString()).update({
+                                                              'isHidden': true,
                                                             });
 
+
+                                                            log(getDocRefs.data().toString());
+                                                            // log(blockListProvider.value.toString());
+                                                            log('hi, I am: ${result.toString()}');
+                                                            log('hi, we are: ${result2.toString()}');
+                                                            // await docRefs2
+                                                            //     .update({
+                                                            //   'THIS IS A TEST': true
+                                                            // });
                                                             Future.delayed(
                                                               Duration.zero,
                                                               () =>
@@ -281,6 +393,7 @@ class Timeline extends ConsumerWidget {
                                                 : null,
 
                                         title: Text(
+                                          //TODO: 後で戻す => commentId
                                           data[index]['comment'].toString(),
                                           style: const TextStyle(
                                             fontSize: 18.0,
@@ -474,6 +587,7 @@ class Timeline extends ConsumerWidget {
                                           'userId': currentUser!.uid,
                                           'location': data.name,
                                           'commentId': documentId,
+                                          'isHidden': false
                                         });
                                         // Navigator.pop(context);
                                       }
